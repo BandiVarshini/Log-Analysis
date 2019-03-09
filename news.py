05cdf9ca84aa58fc3a647e833e3dce2004f22991
BandiVarshini <varshinib98@gmail.com>
2019-03-09 11:14:28 +0530

    initial commit

--- news.py
+++ news.py
@@ -4,7 +4,24 @@ import psycopg2
 DBNAME = "news"
 
 
-query_1 = """
+def connect(database_name="news"):
+    try:
+        db = psycopg2.connect(dbname=DBNAME)
+        cursor = db.cursor()
+        return db, cursor()
+    except:
+        print("<error message>")
+
+
+def get_query_results(query):
+    d = psycopg2.connect(dbname=DBNAME)
+    cursor = d.cursor()
+    cursor.execute(query)
+    obtained = cursor.fetchall()
+    d.close()
+    return obtained
+
+que1 = """
 SELECT articles.title,
        count(*)
 FROM log,
@@ -15,7 +32,17 @@ ORDER BY count(*) DESC
 LIMIT 3;
 """
 
-query_2 = """
+
+def report1():
+    d = psycopg2.connect(dbname=DBNAME)
+    cursor = d.cursor()
+    cursor.execute(que1)
+    obtained = cursor.fetchall()
+    for res in obtained:
+        print('{title} = {count} views'.format(title=res[0], count=res[1]))
+
+
+que2 = """
 SELECT authors.name,
        count(*) AS num
 FROM authors
@@ -27,63 +54,37 @@ GROUP BY authors.name
 ORDER BY num DESC;
 """
 
-query_3 = """
-select * from (
-    select a.day,
-    round(cast((100*b.hits) as numeric) / cast(a.hits as numeric), 2)
-    as errp from
-    (select date(time) as day, count(*) as hits from log group by day) as a
-    inner join
-    (select date(time) as day, count(*) as hits from log where status
-    like '%404%' group by day) as b
-    on a.day = b.day)
-    as t where errp > 1.0;
+
+def report2():
+    obtained = get_query_results(que2)
+    for row in obtained:
+        print('{num1} = {num2} views'.format(num1=row[0], num2=row[1]))
+
+
+que3 = """
+select *
+FROM (SELECT v.day,
+round(cast((100*k.hits) as numeric) / cast(v.hits as numeric), 2)
+as err
+FROM(SELECT date(time) as day,
+count(*) as hits FROM log group by day) as v
+INNER JOIN (SELECT date(time) as day, count(*) as hits FROM log
+where status like '%404%' group by day) as k
+on v.day = k.day)
+as t where err > 1.0;
 """
 
 
-def connect(database_name="news"):
-    try:
-        db = psycopg2.connect(dbname=DBNAME)
-        cursor = db.cursor()
-        return db, cursor()
-    except:
-        print("<error message>")
-
-
-def get_query_results(query):
-    db = psycopg2.connect(dbname=DBNAME)
-    cursor = db.cursor()
-    cursor.execute(query)
-    results = cursor.fetchall()
-    db.close()
-    return results
-
-
-def article(queries):
-    db = psycopg2.connect(dbname=DBNAME)
-    cursor = db.cursor()
-    cursor.execute(query_1)
-    results = cursor.fetchall()
-    for res in results:
-        print('{title} - {count} views'.format(title=res[0], count=res[1]))
-
-
-def author():
-    results = get_query_results(query_2)
-    for row in results:
-        print row[0], "-", row[1], "views"
-
-
-def error():
-    results = get_query_results(query_3)
-    for row in results:
-        print row[0], "-", row[1], "%"
+def Error():
+    obtained = get_query_results(que3)
+    for row in obtained:
+        print('{num1} = {num2} % error '.format(num1=row[0], num2=row[1]))
 
 
 if __name__ == '__main__':
-    print("\nPopular 3 articles\n")
-    article(query_1)
-    print("\npopular 4 authors\n")
-    author()
-    print("\n Days in which more than 1 %")
-    error()
+    print("\nPopular 3 articles report\n")
+    report1()
+    print("\npopular 4 authors report\n")
+    report2()
+    print("\n Days in which more than 1% error")
+    Error()
